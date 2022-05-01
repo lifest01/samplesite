a06e08a2487372bc24b49b0443d05771f5fd0960
lifest <01stas01@gmail.com>
2022-05-01 17:24:15 +0300

    Простая доска объявлений

---
+++ bboard/views.py
@@ -0,0 +1,38 @@
+from django.views.generic.edit import CreateView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Bb, Rubric
+from .forms import BbForm
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    content = {
+        'bbs': bbs,
+        'rubrics':rubrics,
+    }
+    return render(request, 'bboard/index.html', content)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    content = {
+        'bbs': bbs,
+        'rubrics': rubrics,
+        'current_rubric': current_rubric,
+    }
+    return render(request, 'bboard/by_rubric.html', content)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics']=Rubric.objects.all()
+        return context
